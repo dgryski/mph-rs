3c008b18895aaa0c8f53ea4695ab8af1225330a2
Damian Gryski <damian@gryski.com>
2019-12-05 10:02:34 -0800

    move new into Table impl

--- src/lib.rs
+++ src/lib.rs
@@ -20,102 +20,102 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
     s.finish()
 }
 
-pub fn new(keys: &[&str]) -> Table {
-    let size = (keys.len()).next_power_of_two();
-    let mut h: Vec<Vec<Entry>> = Vec::with_capacity(size);
-    for _ in 0..size {
-        h.push(Vec::new())
-    }
-
-    for (idx, k) in keys.iter().enumerate() {
-        let hash = calculate_hash(k);
-        let i = hash % (size as u64);
-        // idx+1 so we can identify empty entries in the table with 0
-        h[i as usize].push(Entry {
-            idx: (idx + 1) as i32,
-            hash,
-        });
-    }
-
-    h.sort_by(|a, b| b.len().cmp(&a.len()));
-
-    let mut values = vec![0i32; size];
-    let mut seeds = vec![0i32; size];
-
-    let mut hidx = 0;
-
-    for idx in 0..h.len() {
-        hidx = idx;
-        if h[hidx].len() <= 1 {
-            break;
+impl Table {
+    pub fn new(keys: &[&str]) -> Table {
+        let size = (keys.len()).next_power_of_two();
+        let mut h: Vec<Vec<Entry>> = Vec::with_capacity(size);
+        for _ in 0..size {
+            h.push(Vec::new())
         }
 
-        let subkeys = &h[hidx];
+        for (idx, k) in keys.iter().enumerate() {
+            let hash = calculate_hash(k);
+            let i = hash % (size as u64);
+            // idx+1 so we can identify empty entries in the table with 0
+            h[i as usize].push(Entry {
+                idx: (idx + 1) as i32,
+                hash,
+            });
+        }
 
-        let mut seed = 0u64;
-        let mut entries: HashMap<usize, i32> = HashMap::new();
+        h.sort_by(|a, b| b.len().cmp(&a.len()));
 
-        'newseed: loop {
-            seed += 1;
-            for k in subkeys.iter() {
-                let i = (xorshift_mult64(k.hash + seed) as usize) % size;
-                if entries.get(&i) == None && values[i] == 0 {
-                    // looks free, claim it
-                    entries.insert(i, k.idx);
-                    continue;
-                }
+        let mut values = vec![0i32; size];
+        let mut seeds = vec![0i32; size];
 
-                // found a collision, reset and try a new seed
-                entries.clear();
-                continue 'newseed;
+        let mut hidx = 0;
+
+        for idx in 0..h.len() {
+            hidx = idx;
+            if h[hidx].len() <= 1 {
+                break;
             }
 
-            // made it through; everything got placed
-            break;
+            let subkeys = &h[hidx];
+
+            let mut seed = 0u64;
+            let mut entries: HashMap<usize, i32> = HashMap::new();
+
+            'newseed: loop {
+                seed += 1;
+                for k in subkeys.iter() {
+                    let i = (xorshift_mult64(k.hash + seed) as usize) % size;
+                    if entries.get(&i) == None && values[i] == 0 {
+                        // looks free, claim it
+                        entries.insert(i, k.idx);
+                        continue;
+                    }
+
+                    // found a collision, reset and try a new seed
+                    entries.clear();
+                    continue 'newseed;
+                }
+
+                // made it through; everything got placed
+                break;
+            }
+
+            // mark subkey spaces as claimed
+            for (&k, &v) in entries.iter() {
+                values[k] = v
+            }
+
+            // and assign this seed value for every subkey
+            for k in subkeys {
+                let i = (k.hash as usize) % size;
+                seeds[i] = seed as i32;
+            }
         }
 
-        // mark subkey spaces as claimed
-        for (&k, &v) in entries.iter() {
-            values[k] = v
+        // find the unassigned entries in the table
+        let mut free: Vec<usize> = Vec::new();
+        for (i, v) in values.iter_mut().enumerate() {
+            if *v == 0 {
+                free.push(i);
+            } else {
+                // decrement idx as this is now the final value for the table
+                *v -= 1;
+            }
         }
 
-        // and assign this seed value for every subkey
-        for k in subkeys {
+        while hidx < h.len() && !h[hidx].is_empty() {
+            let k = &h[hidx][0];
             let i = (k.hash as usize) % size;
-            seeds[i] = seed as i32;
+            hidx += 1;
+
+            // take a free slot
+            let dst = free.pop().unwrap();
+
+            // claim it; -1 because of the +1 at the start
+            values[dst] = k.idx - 1;
+
+            // store offset in seed as a negative; -1 so even slot 0 is negative
+            seeds[i] = -(dst as i32 + 1);
         }
+
+        Table { values, seeds }
     }
 
-    // find the unassigned entries in the table
-    let mut free: Vec<usize> = Vec::new();
-    for (i, v) in values.iter_mut().enumerate() {
-        if *v == 0 {
-            free.push(i);
-        } else {
-            // decrement idx as this is now the final value for the table
-            *v -= 1;
-        }
-    }
-
-    while hidx < h.len() && !h[hidx].is_empty() {
-        let k = &h[hidx][0];
-        let i = (k.hash as usize) % size;
-        hidx += 1;
-
-        // take a free slot
-        let dst = free.pop().unwrap();
-
-        // claim it; -1 because of the +1 at the start
-        values[dst] = k.idx - 1;
-
-        // store offset in seed as a negative; -1 so even slot 0 is negative
-        seeds[i] = -(dst as i32 + 1);
-    }
-
-    Table { values, seeds }
-}
-
-impl Table {
     // Query looks up an entry in the table and return the index.
     pub fn query(&self, k: &str) -> usize {
         let size = self.values.len();
